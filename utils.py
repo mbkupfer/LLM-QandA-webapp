1275125d8f74820ff061045d02dda500f01fa46f
Maxim Kupfer <mbkupfer@gmail.com>
2024-04-24 15:57:59 -0700

    Documentation, make concise

--- utils.py
+++ utils.py
@@ -4,7 +4,16 @@ import urllib.request
 from urllib.error import HTTPError, URLError
 
 
-def extract_logs(urls: List[str]):
+def extract_logs(urls: List[str]) -> str:
+    """Consume, clean, and combine urls pointing to log files
+
+    Args:
+        urls (List[str]): URLs pointing to logfiles
+
+    Returns:
+        str: Combined logs for injection into AI prompt
+    """
+
     content = []
     urls = [url.strip() for url in urls]
     for url in urls:
@@ -13,23 +22,16 @@ def extract_logs(urls: List[str]):
             text = resp.read().decode("utf-8")
 
             # Remove timestamps (e.g., '00:01:11,430 --> 00:01:40,520')
-            try:
-                text = re.sub(
-                    r"\d{2}:\d{2}:\d{2},\d{3} --> \d{2}:\d{2}:\d{2},\d{3}", "", text
-                )
-            except re.error:
-                pass
+            text = re.sub(
+                r"\d{2}:\d{2}:\d{2},\d{3} --> \d{2}:\d{2}:\d{2},\d{3}", "", text
+            )
             # Remove line numbers
-            try:
-                text = re.sub(r"\d+", "", text)
-            except re.error:
-                pass
-
-            # Replace PC style carriage return with unix style new line
+            text = re.sub(r"\d+", "", text)
+            # Replace double blank lines (PC style carriage return or unix style) with single unix style new line
             text = re.sub("(\r\n)+|(\n{2,})", "\n", text)
 
             content.append(text.strip())
 
-        except (ValueError, URLError, HTTPError) as e:
+        except (ValueError, URLError, HTTPError):
             pass
     return "\n".join(content)
